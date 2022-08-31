f0c4a3e576875511621c7fea2ebac8f3d78dab90
Jan Surdziel <surdzieljan1994@gmail.com>
2022-08-31 11:07:51 +0200

    Refactor in ArtistRoute

--- client_bazart/src/Features/ArtistSite/artistSite.jsx
+++ client_bazart/src/Features/ArtistSite/artistSite.jsx
@@ -1,9 +1,11 @@
+import { useParams } from "react-router-dom";
 import ArtistEvents from "./components/artistevents/artistevents.component";
 import ArtistWork from "./components/artistwork/artistwork.component";
 import Banner from "./components/banner/artistbanner.component";
 import Navigation from "./components/navigation/navigation.component";
 
-const ArtistSite = () => {
+const ArtistSite = (props) => {
+  const par = useParams();
     return (
         <div>
             <Navigation />
