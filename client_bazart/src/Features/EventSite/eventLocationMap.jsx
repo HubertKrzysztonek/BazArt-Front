cdc5051fc79a34e774a607a643b6525308fc1704
Hubert Krzysztonek <hubertkrzysztonekk@gmail.com>
2022-08-31 11:33:11 +0200

    event map changed

--- client_bazart/src/Features/EventSite/eventLocationMap.jsx
+++ client_bazart/src/Features/EventSite/eventLocationMap.jsx
@@ -1,46 +1,53 @@
-import { Map, GoogleApiWrapper } from "google-maps-react";
-import { Component } from "react";
+import { useMemo } from "react";
+import {
+  GoogleMap,
+  Marker,
+  MarkerF,
+  useLoadScript,
+} from "@react-google-maps/api";
 
-class EventLocationMap extends Component {
-  render() {
-    return (
-      <div class="location-map-div">
-        <Map
-          google={this.props.google}
-          style={{ width: "50%", height: "50%" }}
-          zoom={15}
-          initialCenter={{
-            lat: 38.4111335,
-            lng: 23.6664712,
-          }}
-          // <Marker position={{38.41,lng: 23.66,}} />
-        />
-      </div>
-    );
-  }
+export default function EventLocationMap() {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyA-SntA_2_IzdI2qFOMRUU7Xr3516LNaTg",
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
 }
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyA-SntA_2_IzdI2qFOMRUU7Xr3516LNaTg",
-})(EventLocationMap);
+function Map() {
+  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+
+  return (
+    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+      <MarkerF position={center} />
+    </GoogleMap>
+  );
+}
+
+// export default EventLocationMap;
 
 // import { Map, GoogleApiWrapper } from "google-maps-react";
+// import { Component } from "react";
 
-// const EventLocationMap = () => {
-//   return (
-//     <div class="location-map-div">
-//       <Map
-//         google={this.props.google}
-//         style={{ width: "100%", height: "100%" }}
-//         zoom={10}
-//         initialCenter={{
-//           lat: 28.70406,
-//           lng: 77.102493,
-//         }}
-//       />
-//     </div>
-//   );
-// };
+// class EventLocationMap extends Component {
+//   render() {
+//     return (
+//       <div class="location-map-div">
+//         <Map
+//           google={this.props.google}
+//           style={{ width: "50%", height: "50%" }}
+//           zoom={15}
+//           initialCenter={{
+//             lat: 38.4111335,
+//             lng: 23.6664712,
+//           }}
+//           // <Marker position={{38.41,lng: 23.66,}} />
+//         />
+//       </div>
+//     );
+//   }
+// }
 
 // export default GoogleApiWrapper({
 //   apiKey: "AIzaSyA-SntA_2_IzdI2qFOMRUU7Xr3516LNaTg",
