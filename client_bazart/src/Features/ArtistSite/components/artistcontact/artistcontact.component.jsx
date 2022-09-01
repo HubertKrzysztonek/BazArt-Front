7538aac4ea2ebb300b70ad0f0b3357b6426459e5
Jan Surdziel <surdzieljan1994@gmail.com>
2022-09-01 10:35:03 +0200

    Added stylying to ArtistSite and refactor few components inside

---
+++ client_bazart/src/Features/ArtistSite/components/artistcontact/artistcontact.component.jsx
@@ -0,0 +1,18 @@
+import './artistcontact.styles.css';
+
+const ArtistContact = () => {
+    return (
+        <div className='contact'>
+            <div className='artist-about-title'>
+                <h1>KONTAKT:</h1>
+            </div>
+
+            <div className='artist-about-description'>
+                <p>Brak</p>
+            </div>
+
+        </div>
+    )
+  };
+  
+  export default ArtistContact;
