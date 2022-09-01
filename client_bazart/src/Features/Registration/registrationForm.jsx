4a90cdb2665d5cba1251c6af7caeef5a66018547
Mateusz Skiba <mateusz.skiba858@gmail.com>
2022-09-01 14:59:48 +0200

    login and register new css

--- client_bazart/src/Features/Registration/registrationForm.jsx
+++ client_bazart/src/Features/Registration/registrationForm.jsx
@@ -1,11 +1,11 @@
 import RedirectButtons from "./redirectButtons";
 import "./registrationForm.css";
-
+import logo from "../images/registerlogo.jpg";
 const RegistrationForm = () => {
   return (
-    <>
-      <RedirectButtons />
-      <div className="container">
+    <div class="main-login-page">
+      {/* <RedirectButtons /> */}
+      {/* <div className="container">
         <form>
           <h1>Login Form</h1>
           <div className="ui divider"></div>
@@ -25,8 +25,51 @@ const RegistrationForm = () => {
             <button className="fluid ui button blue">Submit</button>
           </div>
         </form>
-      </div>
-    </>
+      </div> */}
+      <RedirectButtons />
+      <section class="sign-in">
+        <div class="sign-in-container">
+          <div class="signin-content">
+            <div class="signin-image">
+              <figure>
+                <img src={logo} alt="sing up image" />
+              </figure>
+            </div>
+            <div class="signin-form">
+              <h2 class="sing-in-form-title">Sign up</h2>
+              <form class="sign-form" id="login-form">
+                <div class="form-group">
+                  <input
+                    type="text"
+                    name="your_name"
+                    id="your_name"
+                    placeholder="Your Name"
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="password"
+                    name="your_pass"
+                    id="your_pass"
+                    placeholder="Password"
+                  />
+                </div>
+
+                <div class="form-group form-button">
+                  <input
+                    type="submit"
+                    name="signin"
+                    id="signin"
+                    class="form-submit"
+                    value="Log in"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
