27c73428356f53ac884ca595235702ddf58f5c02
RogerWitkowski <witkowski.roger@gmail.com>
2022-09-01 10:53:53 +0200

    imports added in app.js

--- client_bazart/src/App/Layout/App.js
+++ client_bazart/src/App/Layout/App.js
@@ -5,6 +5,9 @@ import "./App.css";
 import ArtistSite from "../../Features/ArtistSite/artistSite";
 import EventPage from "../../Features/EventSite/eventPage";
 import NavigationMain from "../../Features/navigation-main/navigation.main.component";
+import Registration from "../../Features/Registration/registration";
+import LoginForm from "../../Features/Registration/loginForm";
+import RegistrationForm from "../../Features/Registration/registrationForm";
 
 const App = () => {
   const [categories, setCategory] = useState([]);
