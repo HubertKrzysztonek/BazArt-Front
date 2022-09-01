74f912ca6d812a52e9fcc0d12295fa93da69d5a0
Mateusz Skiba <mateusz.skiba858@gmail.com>
2022-09-01 10:43:19 +0200

    app.js imports

--- client_bazart/src/App/Layout/App.js
+++ client_bazart/src/App/Layout/App.js
@@ -5,6 +5,9 @@ import "./App.css";
 import ArtistSite from "../../Features/ArtistSite/artistSite";
 import EventPage from "../../Features/EventSite/eventPage";
 import NavigationMain from "../../Features/navigation-main/navigation.main.component";
+import Registration from "./../../Features/Registration/registration";
+import LoginForm from "./../../Features/Registration/loginForm";
+import RegistrationForm from "./../../Features/Registration/registrationForm";
 
 const App = () => {
   const [categories, setCategory] = useState([]);
@@ -21,7 +24,7 @@ const App = () => {
         <Route index element={<HomePage categories={categories} />} />
       </Route>
       <Route path="/event/:eventId" element={<EventPage />}></Route>
-      <Route path="/artistSite" element={<ArtistSite />}></Route>
+      <Route path="/user/:userId" element={<ArtistSite />}></Route>
       <Route path="/login" element={<Registration />}></Route>
       <Route path="/login/login" element={<LoginForm />}></Route>
       <Route path="/login/register" element={<RegistrationForm />}></Route>
