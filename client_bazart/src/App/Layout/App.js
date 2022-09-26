2789ba5e2ce8de1a760b0c518201ad31babdc089
Mateusz Skiba <mateusz.skiba858@gmail.com>
2022-09-26 13:27:50 +0200

    registrationForm

--- client_bazart/src/App/Layout/App.js
+++ client_bazart/src/App/Layout/App.js
@@ -8,6 +8,7 @@ import NavigationMain from "../../Features/navigation-main/navigation.main.compo
 import Registration from "./../../Features/Registration/registration";
 import LoginForm from "./../../Features/Registration/loginForm";
 import RegistrationForm from "./../../Features/Registration/registrationForm";
+import Register from "../../Features/RegistrationForm/Register";
 import { AnimatePresence } from "framer-motion";
 const App = () => {
   const [categories, setCategory] = useState([]);
@@ -29,6 +30,7 @@ const App = () => {
         <Route path="/login" element={<Registration />}></Route>
         <Route path="/login/login" element={<LoginForm />}></Route>
         <Route path="/login/register" element={<RegistrationForm />}></Route>
+        <Route path="/registertest" element={<Register />}></Route>
       </Routes>
     </AnimatePresence>
   );
